040962f5cd7b0919d4b1b7315144365f11dd5289
Domincee <daseberos@gmail.com>
2025-05-23 03:14:34 +0800

    added form edit

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,97 @@ import { employees } from "/public/js/record.js";
 
 const totalEmployee = document.getElementById("totalEmployee");
 const employee = document.getElementById("employee");
+let editingIndex = -1;
+
+/* ALERT  */
+const confirmBtns = document.getElementById("confirmBtns");
+const confirmMess = document.getElementById("confirm-message");
+
+
+/* MODALS */
+const formModal = document.querySelector(".form-modal");
+const confiModal = document.getElementById("confirmationModal");
+
+
+
+
+
+/*SHOW FORM EMPLOYEE */
+const addNew = document.querySelector(".add-new");
+const cancelAdd = document.querySelector(".cancel-add");
+ const dateInput = document.getElementById("dateHired");
+addNew.addEventListener("click", function() {
+
+    confiModal.style.display = "none"; 
+    formModal.style.display = "flex"; 
+    formEmp.style.flexDirection = "column";
+   
+     if (dateInput) {
+    dateInput.style.display = "none"; // Hide the date input
+  }
+
+});
+cancelAdd.addEventListener("click", function() {
+
+ formModal.style.display = "none"; 
+});
+
+
+const formEmp = document.getElementById("employeeForm");
+const fNameInput = document.getElementById("firstName");
+const lNameInput = document.getElementById("lastName");
+const depInput = document.getElementById("department");
+
+
+
+
+formEmp.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const firstName = fNameInput.value.trim();
+  const lastName = lNameInput.value.trim();
+  const department = depInput.value.trim();
+
+  if (editingIndex >= 0) {
+    employees[editingIndex] = { firstName, lastName, department, active: true, dateHired: new Date().toLocaleDateString() };
+    editingIndex = -1;
+
+  } else {
+    employees.push({ firstName, lastName, department,active: true, dateHired: new Date().toLocaleDateString() });
+  }
+
+  formEmp.reset();
+  renderTable();
+});
+
+/* ADD MODAL */
+window.confirmDeleteEmployee = function(index) {
+    formModal.style.display = "none"; 
+    const emp = employees[index];
+    confirmMess.textContent = `Are you sure you want to remove ${emp.firstName} ${emp.lastName} from the list?`;
+    const btnYes = confirmBtns.querySelector(".btn-yes");
+    btnYes.onclick = function() {
+   
+
+       setTimeout(() => {
+             confiModal.style.display = "none";
+        }, 300); 
+        employees.splice(index, 1);    
+        renderTable();
+        renderTotal();
+        renderNewHires();  
+    } 
+    
+    const btnNo = confirmBtns.querySelector(".btn-no");
+    btnNo.onclick = function() {
+        setTimeout(() => {
+             confiModal.style.display = "none";
+        }, 300);
+      
+    }
+        
+    confiModal.style.display = "flex"; 
+};
+
 
 
 const newEmpTtl = document.getElementById("newEmpTotal");
@@ -11,8 +102,6 @@ function renderTotal() {
   totalEmployee.textContent = count;
    employee.textContent = count > 1 ? "Employees"   : "Employee";
 
-   
-  
 }
 
 function renderNewHires(){
@@ -31,7 +120,6 @@ function renderNewHires(){
 
 
 const tbBody = document.getElementById("employeeTable");
-
 
 function renderTable() {
     tbBody.innerHTML = "";
@@ -53,46 +141,58 @@ function renderTable() {
         tbBody.appendChild(row);
     });
 
-}
-const confiModal = document.getElementById("confirmationModal");
-const confirmBtns = document.getElementById("confirmBtns");
-const confirmMess = document.getElementById("confirm-message");
 
-window.confirmDeleteEmployee = function(index) {
-    const emp = employees[index];
- 
-    confirmMess.textContent = `Are you sure you want to remove ${emp.firstName} ${emp.lastName} from the list?`;
-    const btnYes = confirmBtns.querySelector(".btn-yes");
-    btnYes.onclick = function() {
-        employees.splice(index, 1);
-        // Re-render the table
-        renderTable();
-        renderTotal();
-        renderNewHires();
-    
-        confiModal.style.display = "none"; // or use classList.remove('show')
-    } 
-    
-    const btnNo = confirmBtns.querySelector(".btn-no");
-    btnNo.onclick = function() {
-        setTimeout(() => {
-             confiModal.style.display = "none";
-        }, 300);
-        // or use classList.remove('show')
+
+}
+
+window.editEmployee = function(index) {
+  console.log("Edit employee at index:", index);
+
+  // Show modal
+  formModal.style.display = "flex";
+  formEmp.style.flexDirection = "row";
+
+  /* Add inputs dateHire*/  
+    if (!document.getElementById("dateHired")) {
+    const dateInput = document.createElement("input");
+    dateInput.type = "date";
+    dateInput.id = "dateHired";
+    dateInput.name = "dateHired";
+    dateInput.required = true;
+
+    // Set max date to today
+    const today = new Date().toISOString().split("T")[0];
+    dateInput.max = today;
+
+    // Insert before buttons div
+    const btnsDiv = formEmp.querySelector(".btns");
+    formEmp.insertBefore(dateInput, btnsDiv);
     }
-        
-    confiModal.style.display = "block"; 
+
+  // Fill inputs
+  const emp = employees[index];
+  document.getElementById("firstName").value = emp.firstName;
+  document.getElementById("firstName").setAttribute("placeholder", emp.firstName);
+  document.getElementById("lastName").value = emp.lastName;
+    document.getElementById("lastName").setAttribute("placeholder", emp.lastName);
+  document.getElementById("department").value = emp.department;
+ document.getElementById("department").setAttribute("placeholder", emp.department);
+  editingIndex = index;
+
+ 
+
 };
+
+
+
+
 
 
 window.cancelDelete = function () {
   confiModal.style.display = "none"; // Or remove "show" class
 };
 
-window.editEmployee = function(index) {
-    console.log("Edit employee at index:", index);
-    console.log("Employee data:", employees[index]);
-};
+
 renderTable();
 renderNewHires();
 renderTotal();
